60bfcbb8bd464c432d96a1cf7a504823f861db90
Pranav Gupta <gpranav588@gmail.com>
2025-10-28 21:42:50 +0530

    Delete Palindrome detecter.py

--- Palindrome detecter.py
+++
@@ -1,8 +0,0 @@
-string = input("Enter a string: ").replace(" ", "").lower()
-
-string1 = string[::-1]
-
-if string == string1:
-    print("The string is a palindrome.")
-else:
-    print("The string is not a palindrome.")
